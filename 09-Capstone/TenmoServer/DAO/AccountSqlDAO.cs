342e38d76864da8924959d5c1ac95456fffbbaf0
maxdsmichael <maxdsmichael@gmail.com>
2020-06-30 15:51:12 -0400

    Implemented models/controller/DAO

---
+++ 09-Capstone/TenmoServer/DAO/AccountSqlDAO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+using TenmoServer.Models;
+
+namespace TenmoServer.DAO
+{
+    public class AccountSqlDAO : IAccountDAO
+    {
+        private readonly string connectionString;
+        private readonly User user;
+
+        public decimal GetBalance()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("Select balance From accounts Where user_id = @user_id", conn);
+                    cmd.Parameters.AddWithValue("@user_id", user.UserId);
+                    decimal balance = Convert.ToDecimal(cmd.ExecuteScalar());
+                    return balance;
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+        }
+    }
+}
